11eab08b794a42b8af9239b268ca51109e2a0fd3
MatiasVasquezMontecinos <matiasvaquez18@gmail.com>
2025-06-03 21:10:14 -0400

    Creacion de los estilos basicos para el login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,17 @@
+import styles from './Login.module.css'
+
 const Login = () => {
     return (
-        <section>
+        <section className={styles.container}>
+            <h2>Incio de Sesion</h2>
             <form action="#">
-                <div>
+                <div className={styles.field}>
                     <label htmlFor="email">Correo Electronico</label>
-                    <input type="email" id='email' name="email" required/>
+                    <input type="email" id='email' name="email" placeholder='Ingrese su correo electronico' required/>
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label htmlFor="password">Contraseña</label>
-                    <input type="password" id="password" name="password" required/>
+                    <input type="password" id="password" name="password" placeholder='Ingrese su contraseña'required/>
                 </div>
                 <button type="sumbit">Iniciar Sesion</button>
             </form>
