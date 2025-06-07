754893ef1fc648cdb28bb12769f63fe0ce9a248e
MatiasVasquezMontecinos <matiasvaquez18@gmail.com>
2025-06-07 14:30:01 -0400

    cambio en la escritura de un archivo para el pmv

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 export const supabase_env = {
     SUPABASE_URL : process.env.SUPABASE_URL || "",
-    SUPABASE_ANON_KEY : process.env.UPABASE_ANON_KEY || "",
+    SUPABASE_ANON_KEY : process.env.SUPABASE_ANON_KEY || "",
 }
